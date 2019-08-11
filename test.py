42fd9e87ccae8c938aca87231452db5184da713d
Dreamoon <niujin98@163.com>
2019-08-12 00:40:01 +0800

    test1

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ from rasa_nlu.training_data import load_data
 from rasa_nlu.config import RasaNLUModelConfig
 from rasa_nlu.model import Trainer
 from rasa_nlu import config
+
+trainer = Trainer(config.load("config_spacy.yml"))
+training_data = load_data('training_data_rasa.json')
+interpreter = trainer.train(training_data)
