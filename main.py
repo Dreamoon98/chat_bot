aba5005b67dd64539c5cc2e25ef6fcc62a90388f
Dreamoon <niujin98@163.com>
2019-08-14 01:55:58 +0800

    2

--- main.py
+++ main.py
@@ -18,22 +18,64 @@ interpreter=trainer()
 #state
 INIT=0
 INTRO=1
-ASK=2
-ASK_DATE=3
-ASK_MORE=4
-THANK=5
+ASK_COMPANY=2
+ASK_SPE=3
+THANK=4
 
 policy_rules = {
+    '''
     (INIT, "none"): (INIT, "I'm sorry but I'm not sure how to help you"),
     (INIT, "greet"): (INTRO,""),
-    (INIT, "get_sth"): (ASK,""),
-    (INIT, "get_history_price"): (ASK_DATE,""),
-    (INIT, "get_current_price"): (ASK,""),
-    (INIT, "get_volumn"): (ASK,""),
-    (INIT, "get_cap"): (ASK,""),
-    (INIT, "get_cap"): (ASK, ""),
+    (INIT, "get_company"): (ASK_SPE,""),
+    (INIT, "get_stock"): (ASK_SPE,""),
+    (INIT, "tks"): (THANK,"I'm glad to help you!"),
 
-    (ASK_DATE,None):(ASK_MORE,"What else do you want to know?",None),
+    (INTRO, "none"):(INIT, "I'm sorry but I'm not sure how to help you"),
+    (INTRO, "greet"): (INTRO,""),
+    (INTRO, "get_company"): (ASK_COMPANY,""),
+    (INTRO, "get_stock"): (ASK_SPE,""),
+    (INTRO, "tks"): (THANK, "I'm glad to help you!"),
+    '''
+
+    (INIT, "none"): INIT,
+    (INIT, "greet"): INTRO,
+    (INIT, "tks"): THANK,
+
+    (INTRO, "get_company"): ASK_COMPANY,
+    (INTRO, "get_stock"): ASK_SPE,
+    (INTRO, "tks"): THANK,
+
+    (ASK_COMPANY, "get_company"): ASK_SPE,
+    (ASK_COMPANY, "get_stock"): ASK_SPE,
+    (ASK_COMPANY, "tks"): THANK,
+
+    (ASK_SPE, "get_stock"): ASK_SPE,
+    (ASK_SPE, "get_company"): ASK_SPE,
+    (ASK_COMPANY, "tks"): THANK,
+
 }
+bot_re = {
+    'intro':"I'm the robot that can get stock information to you!",
+    'get_company':"Which stock do you want to know?",
+    'thank':"I'm glad to help you!",
+    'open':"Open price on {}: {}",
+    'volume':"Volume on {}: {}",
+    'cap':"Market cap on {}: {}",
+    'current_price':"Current price on {}: {}",
+    'default':"I'm sorry but I'm not sure how to help you"
+}
+
+def interpret(message):
+    intent=interpreter.parse(message)
+    entities = interpreter.parse(message)["entities"]
+    return ''
+def send_message(policy, state, message):
+    print("USER : {}".format(message))
+    new_state, response = respond(policy, state, message)
+    print("BOT : {}".format(response))
+    return new_state
+
+def respond(policy, state, message):
+    pass
 
 
